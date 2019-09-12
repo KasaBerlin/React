932657a0910e7b3e84030074157c89b9b52130dd
pudelgulasch <katrinsalac@gmail.com>
2019-09-12 12:59:34 +0200

    add

--- event-handling/src/index.js
+++ event-handling/src/index.js
@@ -27,50 +27,82 @@ import ReactDOM from "react-dom";
 
 // ReactDOM.render(<Toggle />, document.getElementById("root"));
 
-class App extends React.Component {
-  state = {
-    // Initial State - Ausgangszustand
-    input: "",
-    reversedText: ""
-  };
-  // function to set a new state for the input
-  handleChange = e => {
-    const value = e.target.value;
-    this.setState({ input: value });
-  };
-  handleReverse = e => {
-    e.preventDefault();
-    const text = this.state.input;
-    this.setState({
-      reversedText: text
-        .split("")
-        .reverse("")
-        .join("")
-    });
-  };
+// class App extends React.Component {
+//   state = {
+//     // Initial State - Ausgangszustand
+//     input: "",
+//     reversedText: ""
+//   };
+//   // function to set a new state for the input
+//   handleChange = e => {
+//     const value = e.target.value;
+//     this.setState({ input: value });
+//   };
+//   handleReverse = e => {
+//     e.preventDefault();
+//     const text = this.state.input;
+//     this.setState({
+//       reversedText: text
+//         .split("")
+//         .reverse("")
+//         .join("")
+//     });
+//   };
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <form onSubmit={this.handleReverse}>
+//           <div>
+//             <label>Text: {this.state.input}</label>
+//           </div>
+//           <div>
+//             <input
+//               type="text"
+//               value={this.state.input}
+//               onChange={this.handleChange}
+//               placeholder="ENTER TEXT"
+//             />
+//           </div>
+//           <div>
+//             <button>Reverse Text</button>
+//           </div>
+//         </form>
+//         <p>Reversed Text: {this.state.reversedText}</p>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <button onClick={() => this.sayName()}>What is my name</button>;
+//   }
+//   sayName() {
+//     alert(this.props.name);
+//   }
+// }
+
+// ReactDOM.render(<Welcome name="2Pac" />, document.getElementById("root"));
+
+// React doesn't bind behind the scenes -
+// let onClick=this.sayName
+// onClick()
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.boundsayName = () => this.sayName();
+  }
   render() {
-    return (
-      <React.Fragment>
-        <form onSubmit={this.handleReverse}>
-          <div>
-            <label>Text: {this.state.input}</label>
-          </div>
-          <div>
-            <input
-              type="text"
-              value={this.state.input}
-              onChange={this.handleChange}
-              placeholder="ENTER TEXT"
-            />
-          </div>
-          <div>
-            <button>Reverse Text</button>
-          </div>
-        </form>
-        <p>Reversed Text: {this.state.reversedText}</p>
-      </React.Fragment>
-    );
+    return <button onClick={this.boundsayName}>What's my name?</button>;
+  }
+  sayName() {
+    alert(this.props.name);
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Welcome name="2Pac" />, document.getElementById("root"));
+
+// IF you do not need state or binding event handlers, use a functional component

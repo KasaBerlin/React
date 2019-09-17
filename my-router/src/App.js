9b1211f0020cba51fad3592f47f60037f2c74248
pudelgulasch <katrinsalac@gmail.com>
2019-09-17 15:56:31 +0200

    add

--- my-router/src/App.js
+++ my-router/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, Link } from "react-router-dom";
 
 import Start from "./components/Start";
 import Projects from "./components/Projects";
@@ -13,9 +13,9 @@ function App() {
       <header className="App-header">
         <h1>Router Example</h1>
         <nav>
-          <a href="/">Start</a>
-          <a href="Projects">Projects</a>
-          <a href="Products">Products</a>
+          <Link to="/">Start</Link>
+          <Link to="Projects">Projects</Link>
+          <Link to="Products">Products</Link>
         </nav>
         <div className="content">
           <Switch>

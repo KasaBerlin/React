033217e1ceb4319db9dceefc49eec6c95bcf7a74
pudelgulasch <katrinsalac@gmail.com>
2019-09-19 21:40:01 +0200

    add

--- blog/src/components/ShowAllPosts.js
+++ blog/src/components/ShowAllPosts.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 class ShowAllPosts extends React.Component {
-  outputPost = props => {
-    Object.keys(this.props.items).map(uuid => {
-      console.log(this.props.items[uuid].comment);
-      return <div>{this.props.items[uuid].comment}</div>;
-    });
-  };
-
-  render() {
-    return <div>{this.outputPost()}</div>;
-  }
-}
+   render() {
+    return (
+<div>
+<h2>Good morning Developers!</h2>
+{Object.keys(this.props.items).map(uuid => {
+return <div className="post" key={uuid}>
+<p className="titel">{this.props.items[uuid].title}</p>
+<p className="comment">{this.props.items[uuid].comment}</p>
+<p className="userdate">{this.props.items[uuid].user} {this.props.items[uuid].date}</p>
+</div>   
+})}
+</div>
+    )}}
 
 export default ShowAllPosts;

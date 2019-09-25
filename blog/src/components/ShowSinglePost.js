67f3c27ed8465b7c1f38f00ca7b92d6f477db807
pudelgulasch <katrinsalac@gmail.com>
2019-09-25 15:26:33 +0200

    add

--- blog/src/components/ShowSinglePost.js
+++ blog/src/components/ShowSinglePost.js
@@ -1,46 +1,48 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-class ShowSinglePost extends React.Component {
-  userInput = React.createRef();
-  titleInput = React.createRef();
-  commentInput = React.createRef();
-  handleSubmit = e => {
+const ShowSinglePost = props => {
+  // create a ref Object, access with current
+  const userInput = React.createRef();
+  const titleInput = React.createRef();
+  const commentInput = React.createRef();
+  // console.log(userInput);
+  const handleSubmit = e => {
     e.preventDefault();
-    const user = this.userInput.current.value.trim();
-    const title = this.titleInput.current.value.trim();
-    const comment = this.commentInput.current.value.trim();
-    this.props.addPost(user, title, comment);
+    const user = userInput.current.value.trim();
+    const title = titleInput.current.value.trim();
+    const comment = commentInput.current.value.trim();
+    props.addPost(user, title, comment);
     e.currentTarget.reset();
-    this.props.history.push("/showallposts");
+    props.history.push("/showallposts");
   };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Username:</label>
-          <input type="text" ref={this.userInput} required></input>
-          <br />
-          <label>Title:</label>{" "}
-          <input type="text" ref={this.titleInput} required></input>
-          <br />
-          <label>Content:</label>
-          <textarea
-            rows="5"
-            cols="90"
-            ref={this.commentInput}
-            required
-          ></textarea>
-          <br />
-          <button type="submit">
-            <i className="fas fa-envelope-open-text"></i>
-          </button>
-          <br />
-        </form>
-      </div>
-    );
-  }
-}
+  // When a ref is passed to an element in render,
+  // a reference to the node becomes accessible at the current attribute of the ref.
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input
+          type="text"
+          ref={userInput}
+          /* call the ref with the 'ref' attribute */ required
+        ></input>
+        <br />
+        <label>Title:</label>{" "}
+        <input type="text" ref={titleInput} required></input>
+        <br />
+        <label>Content:</label>
+        <textarea rows="5" cols="90" ref={commentInput} required></textarea>
+        <br />
+        <button type="submit">
+          <i className="fas fa-envelope-open-text"></i>
+        </button>
+        <br />
+      </form>
+    </div>
+  );
+};
 
 export default withRouter(ShowSinglePost);
